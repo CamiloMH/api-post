e358fc8696709839625ce3a871953a68548b4e72
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-22 12:47:27 -0300

    Cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors');
 //Creamos el servidor express
 const app = express();
 
-
+//Configurar cors
+app.use(cors());
 
 //Corremos el servidor en el puerto 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server running on port ${process.env.PORT}`);
 }); 
