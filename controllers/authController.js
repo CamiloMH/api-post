2451b317a265778bbe9a9e847fa368c049b30841
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-23 18:15:09 -0300

    Auth

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { tokenJWT } = require('../helpers/jwt');
+
+const login = async(request,response) => {
+	const { mail , password } = request.body;
+
+	try {
+        
+		const user = await User.findOne({ mail });
+		if (!user) {
+			return response.status(404).json({
+				error: 'invalid user or password'
+			});
+		}
+		/* Verificar contraseña */
+		const validPassword = bcrypt.compareSync(password, user.password);
+		if (!validPassword) {
+			return response.status(400).json({
+				error: 'invalid user or password'
+
+			});
+		}
+		
+		const token = await tokenJWT(user.id, user.name);
+
+		response.json({
+			name: user.name,
+			mail: user.mail,
+			token
+		});
+
+
+	} catch (error) {
+		console.log(error);
+		response.sendStatus(404);
+	}
+
+};
+
+module.exports = {
+	login
+};
