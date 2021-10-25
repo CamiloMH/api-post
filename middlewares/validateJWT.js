81dd3a798c6645484d5fa1d0ebe17fcfbde9d17d
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-25 12:12:47 -0300

    Fix

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -2,24 +2,31 @@ const jwt = require('jsonwebtoken');
 
 const validateJWT = (request,response,next) => {
 	const authorization = request.get('authorization');
+	if(!authorization) return response.status(401).json({error: 'Null token'});
 	let token = '';
 
-	console.log(authorization);
-	if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-		token = authorization.substring(7);
+	try {
+		
+	
+		if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+			token = authorization.substring(7);
+		}
+	
+		const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({ error: 'token missing or invalid' });
+		}
+	
+		const { id: userId } = decodedToken;
+	
+		request.userId = userId;
+	
+		next();
+	} catch (error) {
+		return response.status(401).json({
+			error: 'Invalid token'
+		});
 	}
-
-	const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-	console.log(decodedToken);
-	if (!token || !decodedToken.id) {
-		return response.status(401).json({ error: 'token missing or invalid' });
-	}
-
-	const { id: userId } = decodedToken;
-
-	request.userId = userId;
-
-	next();
 };
 
 
