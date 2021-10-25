9bb07ae957c078459557caf8f82b636f257fa994
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-25 14:29:56 -0300

    CRUD

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,23 +3,29 @@ const User = require('../models/User');
 
 /* Obtener todos los post */
 const getPosts = async(request,response) => {
-	const posts = await Post.find({});
+	const posts = await Post.find({}).populate('user',{
+		name:1,
+		lastName:1
+	});
 	response.json(posts);
 };
 
 /* Obtener un post */
 const getPostById = async(request,response) => {
-	const { id } = request.params;
 	try {
-		const postDB = await Post.findById(id);
+		const { id } = request.params;
+		const postDB = await Post.findById(id).populate('user',{
+			name:1,
+			lastName:1
+		});
 		if(postDB === null) return response.sendStatus(400);
 
 		response.json({
-			ok:true,
 			post: postDB
 		});
 	} catch (error) {
-		response.status(400).json();
+		response.sendStatus(error);
+
 	}
 };
 
@@ -28,27 +34,73 @@ const createPost = async(request,response) =>{
 	const { content, important = false} = request.body;
     
 	/* obtener el id del user */
-	console.log(request);
+	const { userId } = request;
+	try {
 
-	// const user = await User.findById(userId);
-	// if(!content) return response.status(400).json({error:'required "content" field is missing'});
+		const user = await User.findById(userId);
+		if(!content) return response.status(400).json({error:'required "content" field is missing'});
     
-	// const newPost = new Post({
-	// 	content,
-	// 	date: new Date(),
-	// 	important,
-	// 	user: user._id
-	// });
+		const newPost = new Post({
+			content,
+			date: new Date(),
+			important,
+			user: user._id
+		});
 
-	// try {
-	// 	const savedPost = await newPost.save();
+		const savedPost = await newPost.save();
 
-	// 	response.json(savedPost);
+		response.json(savedPost);
         
 
-	// } catch (error) {
-	// 	response.sendStatus(400);
-	// }
+	} catch (error) {
+		response.sendStatus(error.name);
+
+	}
+
+};
+
+//Borrar un post
+const deletePost = async(request,response) => {
+	try {
+		const { id } = request.params;
+		const res = await Post.findByIdAndDelete(id);
+		if( res === null) return response.sendStatus(404);
+	
+		response.status(204).end();
+		
+	} catch (error) {
+		response.sendStatus(error.name);
+		
+	}
+};
+
+//Actualizar un post
+const updatePost = async (request,response) => {
+	try {
+		const { id } = request.params;
+		const post = await Post.findById(id);
+		if(!post) return response.status(404).json({
+			error: 'Id not found'
+		});
+		
+		const postInfo = request.body;
+
+		const newPost = {
+			content: postInfo.content,
+			important: postInfo.important,
+		};
+
+		const newPostInfo = await Post.findByIdAndUpdate(id, newPost,{new:true});
+
+		response.json({
+			post: newPostInfo
+		});
+
+		
+	} catch (error) {
+		response.sendStatus(error.name);
+
+	}
 
 };
 
@@ -56,4 +108,6 @@ module.exports = {
 	getPosts,
 	getPostById,
 	createPost,
+	deletePost,
+	updatePost
 };
