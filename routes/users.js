b147b1f937f5ccce16ff60fad64aa5a0ac912b2d
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-25 16:55:13 -0300

    Validate createUser

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@
 const { Router } = require('express');
 const { getUsers, createUser, removeUser, updateUser, getUser} = require('../controllers/usersController');
 const { validateJWT } = require('../middlewares/validateJWT');
+const { userValidator } = require('../Validators/user');
 
 //Inicializamos router
 const router = Router();
@@ -11,8 +12,8 @@ const router = Router();
 //Rutas
 router.get('/',validateJWT,getUsers);
 router.get('/:id',validateJWT,getUser);
-router.post('/',createUser);
+router.post('/',userValidator,createUser);
 router.delete('/:id',validateJWT, removeUser);
-router.put('/:id',validateJWT,updateUser);
+router.put('/:id',validateJWT,updateUser);  
 
 module.exports = router;
