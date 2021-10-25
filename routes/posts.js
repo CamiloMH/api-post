b9c8b21482e225172d81fcd5528e84d9a90ed223
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-25 14:30:24 -0300

    Route posts, Validate JWT

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,18 @@
-/* 
-    Ruta: /api/v1/posts
-*/
+// /* 
+//     Ruta: /api/v1/posts
+// */
 const { Router } = require('express');
+const { createPost, getPosts, getPostById, deletePost, updatePost} = require('../controllers/postController');
+const { validateJWT } = require('../middlewares/validateJWT');
 
 //Inicializamos router
 const router = Router();
 
 //Rutas
-router.get('/');
-router.post('/');
-router.delete('/:id',);
-router.put('/:id');
+router.get('/',getPosts);
+router.get('/:id',getPostById);
+router.post('/',validateJWT,createPost);
+router.delete('/:id',validateJWT, deletePost);
+router.put('/:id',validateJWT,updatePost);
 
 module.exports = router;
